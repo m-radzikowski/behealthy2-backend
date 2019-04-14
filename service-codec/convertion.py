f149ef1f19f373a8599e319c6c8b026cc3877ffb
gabrys <cmateuszm@gmail.com>
2019-04-14 06:25:34 +0200

    changes to codecs

--- service-codec/convertion.py
+++ service-codec/convertion.py
@@ -10,6 +10,7 @@ import base64
 from utils.validation import Validator
 import array
 import pickle
+from time import sleep
 
 #your_rest_server_port = 5001
 # The flowing code will register your server to eureka server and also start to send heartbeat every 30 seconds
@@ -37,18 +38,21 @@ class CodecHandler(Resource):
             file_name = "audio" + str(iterator) + ".wav"
             command = "ffmpeg -i audio.webm -ab 160k -ac 1 -ar 44100 -vn " + file_name
             subprocess.call(command, shell=True)
-            byte_array = array.array('B')
-            audio_file = open(file_name, 'rb')
-            byte_array.fromstring(audio_file.read())
-            new_str = byte_array.tobytes()
+            with open(file_name, 'rb') as params_source:
+                params = params_source.read()
+            params = bytearray(params)
+            del params[0]
+            params_encoded = base64.b64encode(params)
 
-            json = pickle.dumps(audio_file.read())
+            #audio_file = open(file_name, "rb")
+            #enc = base64.b64encode(audio_file.read())
+            #audio_file.close()
 
             to_send = dict(
-                bytes = new_str
+                audio = params_encoded
             )
 
-            return send_file(new_str, attachment_filename=file_name)
+            return to_send, 200
         return 'Internal api server issue', 500
 
 api.add_resource(CodecHandler, '/codec')
